d3731376f2989fc637e3cdececb8e8f96428ac33
Sprayman-FIIIOwner <sprayman1234555@gmail.com>
2026-03-26 11:18:43 +0700

    Added offline.html, updated sw.js to cache it.

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = 'portfolio-v1';
+const ASSETS_TO_CACHE = [
+  'offline.html',
+  'style.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Caching Offline Page');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Biarkan browser ngambil data kayak biasa
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          return cache.match(OFFLINE_URL);
+        });
+      })
+    );
+  }
 });
